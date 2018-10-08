ca70113be7240748772e921a48567bf043b3f321
rajat-rjt <rajat.gupta2017@vitstudent.ac.in>
2018-10-08 14:10:10 +0530

    Gaussian filter added

---
+++ Image Processing.py
@@ -0,0 +1,42 @@
+import cv2
+from cv2 import cv
+import numpy as np
+
+import sys
+
+
+# gaussian filter to blur the image(reducing the noise)
+def gaussian_blur(img):
+    return cv2.GaussianBlur(img (3,3),0,0)
+
+
+
+    
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
