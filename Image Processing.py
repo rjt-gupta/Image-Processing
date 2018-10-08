f51c9288fab8a4218af84d8874b3a58cabf4c2ad
rajat-rjt <rajat.gupta2017@vitstudent.ac.in>
2018-10-08 14:32:10 +0530

    Axis Gradient added

--- Image Processing.py
+++ Image Processing.py
@@ -9,6 +9,15 @@ import sys
 def gaussian_blur(img):
     return cv2.GaussianBlur(img (3,3),0,0)
 
+# Sobel filter (input, output, img_prop*) to calc x gradient
+def x_grad(img):
+    return cv2.Sobel(img, cv2.CV_64F,1,0, ksize=3, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
+
+
+# Calc y gradient from Sobel filter 
+def y_grad(img):
+
+    return cv2.Sobel(img, cv2.CV_64F,0,1, ksize=3,scale=1, delta=0, borderType=BORDER_DEFAULT)
 
 
     
